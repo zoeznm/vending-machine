7678a34e2077117385c3f08979b63349faa7c268
zoeznm <bomikim046@gmail.com>
2024-06-28 16:56:12 +0900

    가로등 수정 중

--- design/de.js
+++ design/de.js
@@ -41,6 +41,40 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+const starLimit = 20;
 
+const lampClasses = [
+    {  light: 'radiationlamplight', imgClass: 'radiation_rock', imgSrc: '/img/radiation_rock.png' },
+    { lamp: 'crimelamp', light: 'crimelamplight', imgClass: 'crime_rock', imgSrc: '/img/crime_rock.png' },
+    { lamp: 'animallamp', light: 'animallamplight', imgClass: 'animal_rock', imgSrc: '/img/animal_rock.png' },
+    { lamp: 'timelamp', light: 'timelamplight', imgClass: 'time_rock', imgSrc: '/img/time_rock.png' },
+    { lamp: 'spacelamp', light: 'spacelamplight', imgClass: 'space_rock', imgSrc: '/img/space_rock.png' }
+];
 
+lampClasses.forEach(lampClass => {
+    document.querySelector(`.${lampClass.light}`).addEventListener('click', () => {
+        const starsContainer = document.getElementById('stars-container');
+        starsContainer.innerHTML = ''; // 기존 별들을 모두 삭제
+        starsContainer.style.opacity = 1; // 별들을 보이도록 설정
 
+        for (let i = 0; i < starLimit; i++) {
+            const star = document.createElement('div');
+            star.className = 'star';
+            star.innerHTML = `<img draggable="true" class="${lampClass.imgClass}" src="${lampClass.imgSrc}" alt="">`;
+
+            // 무작위 위치 설정
+            const endY = getRandomInt(700, 685);
+            const endX = getRandomInt(80, 1500);
+            const rotateAngle = getRandomInt(-90, 90);
+
+            star.style.transform = `translateY(${endY}px) translateX(${endX}px) rotate(${rotateAngle}deg)`;
+            star.style.transition = 'transform 5s ease-in-out';
+
+            starsContainer.appendChild(star);
+        }
+    });
+});
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
